b11abfb9b4e672f674e7e6207b992c0034ecfd37
Jaroslaw Krefta <jaroslaw.krefta@powel.com>
2016-11-09 13:29:28 +0100

    There are no namespaces in js - only modules

--- reporter/src/assets.js
+++ reporter/src/assets.js
@@ -37,6 +37,6 @@ export const wisdom = [
   'Although never is often better than right now.',
   'If the implementation is hard to explain, it\'s a bad idea.',
   'If the implementation is easy to explain, it may be a good idea.',
-  'Namespaces are one honking great idea - let\'s do more of those!',
+  'Modules are great idea - let\'s do more of those!',
   'Nobody ever expects the Spanish Inquisition.'
 ];
